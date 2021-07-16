e8510bbd9305485c528c9c22311f0fce3d0d4d5e
Ahmed Elesdody <ahmed.alasudi@vodafone.com>
2021-07-16 14:05:21 +0200

    init hilt create retrofit

---
+++ app/src/main/java/com/news/data/sharedpreferences/SharedPreferencesStorage.kt
@@ -0,0 +1,5 @@
+package com.news.data.sharedpreferences
+
+interface SharedPreferencesStorage {
+
+}
---
+++ app/src/main/java/com/news/data/sharedpreferences/SharedPreferencesStorageImp.kt
@@ -0,0 +1,6 @@
+package com.news.data.sharedpreferences
+
+import android.content.SharedPreferences
+
+class SharedPreferencesStorageImp :SharedPreferencesStorage {
+}
